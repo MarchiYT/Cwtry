5f85d375d8961eaa197e23740f8ac25bb6bc5822
Marchi <kistin2003@mail.ru>
2023-05-26 20:04:44 +0700

    yunuygfn
    ngfyun

--- test/test.cpp
+++ test/test.cpp
@@ -7,20 +7,12 @@ CTEST (ctest, zeroGridCheck) {
 	int Gridc[12][12];
     bool checkzeroGridTrue = zeroGrid(Gridp, Gridc) ;
     ASSERT_TRUE(!checkzeroGridTrue);
-	
-
-    //bool checkzeroGridFalse = zeroGrid(int Gridp[size + 2][size + 4], int Gridc[size + 3][size + 2]) ;
-    //ASSERT_FALSE(checkzeroGridFalse);
 }
 
 CTEST (ctest, Ship_placementCheck) {
 	int b[12][12];
     bool checkShip_placementTrue = Ship_placement(b, 12);
     ASSERT_TRUE(!checkShip_placementTrue);
-    
-
-    //bool checkShip_placementFalse = Ship_placement(int b[size + 3][size + 4], int size);
-    //ASSERT_FALSE(checkShip_placementFalse);
 }
 
 CTEST (ctest, aroundHitCheck) {
@@ -29,10 +21,6 @@ CTEST (ctest, aroundHitCheck) {
 	int y = 6;
     bool aroundHitTrue = aroundHit(Grid, x, y) ;
     ASSERT_TRUE(!aroundHitTrue);
-    
-
-    //bool aroundHitFalse = aroundHit(int Grid[size + 3][size + 2], int x) ;
-    //ASSERT_FALSE(aroundHitFalse);
 }
 
 CTEST (ctest, timerCheck) {
