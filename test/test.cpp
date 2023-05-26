3264f1a6a51f4c52180702beee47ea78db40ff24
Marchi <kistin2003@mail.ru>
2023-05-26 14:37:19 +0700

    fghfg
    fghfgfg

--- test/test.cpp
+++ test/test.cpp
@@ -1,14 +1,18 @@
 #include "../thirdparty/ctest.h"
 
-#include <Audio.h>
-#include <logic.h>
+#include "../src/game15_logic/game15_logic.h"
+#include "../src/game15_graph/game15_graph.h"
 
-CTEST (ctest, zeroGridCheck) {
-    bool checkzeroGridTrue = zeroGrid(int Gridp[size + 2][size + 2], int Gridc[size + 2][size + 2]) ;
+/*CTEST (ctest, zeroGridCheck) {
+	int Gridp[size + 2][size + 2];
+	int Gridc[size + 2][size + 2];
+	int Gridpf[size + 3][size + 4];
+	int Gridcf[size + 4][size + 2];
+    bool checkzeroGridTrue = zeroGrid(Gridp, Gridc) ;
     ASSERT_TRUE(checkzeroGridTrue);
     
 
-    bool checkzeroGridFalse = zeroGrid(int Gridp[size + 2][size + 4], int Gridc[size + 3][size + 2]) ;
+    bool checkzeroGridFalse = zeroGrid(Gridpf, Gridcf) ;
     ASSERT_FALSE(checkzeroGridFalse);
 }
 
@@ -23,7 +27,7 @@ CTEST (ctest, Ship_placementCheck) {
 
 CTEST (ctest, aroundHitCheck) {
     bool aroundHitTrue = aroundHit(int Grid[size + 2][size + 2], int x, int y) ;
-    ASSERT_TRUE(aroundHitTrue);
+   ASSERT_TRUE(aroundHitTrue);
     
 
     bool aroundHitFalse = aroundHit(int Grid[size + 3][size + 2], int x) ;
@@ -31,10 +35,20 @@ CTEST (ctest, aroundHitCheck) {
 }
 
 CTEST (ctest, computerHitCheck) {
-    bool computerHitTrue = computerHit(int Grid[size + 2][size + 2], int hits, int& x, int& y) ;
+   bool computerHitTrue = computerHit(int Grid[size + 2][size + 2], int hits, int& x, int& y) ;
     ASSERT_TRUE(computerHitTrue);
     
 
     bool computerHit = computerHit(int Grid[size + 3][size + 2], int& x, int& y) ;
     ASSERT_FALSE(computerHitFalse);
+}*/
+CTEST (ctest, loadFromFile) {
+    Texture screenTrue;
+    bool checkScreenTrue = screenTrue.loadFromFile("../external/images/sea.jpg") ;
+    ASSERT_TRUE(checkScreenTrue);
+    
+
+    Texture screenFalse;
+    bool checkScreenFalse = screenFalse.loadFromFile("../external/images/shfghea.jpg") ;
+    ASSERT_FALSE(checkScreenFalse);
 }
